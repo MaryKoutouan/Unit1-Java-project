050fd233161937189478b84552931edb6701008f
BotyK <kooutouanboty@gmail.com>
2022-09-22 16:28:54 -0500

    first java project unit1

---
+++ GuessingGame.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+        public GuessingGame()  {
+
+        }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean correctGuess = false;
+        int guess = 0;
+        int increment = 0;
+
+        System.out.println(" Hello user. What is your name?");
+        String name = input.nextLine();
+
+        System.out.println(" Welcome " +  name + " I am thinking of a number between 1 and 100.");
+        Random rand = new Random();
+        int nextNumber = rand.nextInt(100);//should store this one in a variable.
+
+        System.out.println(" Try to guess my number.");
+        while (correctGuess == false) {
+            increment = increment + 1;
+            try {
+                guess = input.nextInt();
+
+            } catch (Exception e) {
+                String badInput = input.next();
+                System.out.println(" That is not  an integer, try again! ");
+                continue;
+            }
+
+            if (guess > 100 || guess < 1) {
+                System.out.println("Your guess is out of range. Please try again.");
+
+            } else if (guess > nextNumber) {
+                System.out.println("Your guess is too high. Please try again");
+
+            } else if (guess < nextNumber) {
+                System.out.println("Your guess is too low. Please try again");
+
+            } else if (guess == nextNumber) {
+                System.out.println("\nWell done, " + name + "!" + " You found my number in increment tries!");
+                correctGuess = true;
+            }
+        }
+    }
+}
